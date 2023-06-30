3ffb9c61fde29fd95f1bc9472d180a00cbadc4a6
dieterdecooman <73192452+dieterdecooman@users.noreply.github.com>
2023-06-30 23:18:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,33 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pandas as pd
 
-"""
-# Welcome to Streamlit!
+# Create a DataFrame to store registered participants
+participants_df = pd.DataFrame(columns=['Name', 'Date'])
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Sample data for rides
+rides_data = [
+    {'Date': '2023-07-01', 'Location': 'Park A'},
+    {'Date': '2023-07-05', 'Location': 'Park B'},
+    {'Date': '2023-07-10', 'Location': 'Park C'}
+]
+rides_df = pd.DataFrame(rides_data)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Streamlit application header
+st.title("Cycling Calendar and Registration")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Display the table of rides with registration button
+st.subheader("Upcoming Rides")
+for index, row in rides_df.iterrows():
+    ride_date = row['Date']
+    ride_location = row['Location']
+    register_button = st.button(f"Register for {ride_date} - {ride_location}")
+    
+    if register_button:
+        participant_name = st.text_input("Enter your name")
+        if participant_name:
+            participants_df = participants_df.append({'Name': participant_name, 'Date': ride_date}, ignore_index=True)
+            st.success(f"{participant_name} successfully registered for {ride_date} - {ride_location}")
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Display registered participants
+st.subheader("Registered Participants")
+st.dataframe(participants_df)
